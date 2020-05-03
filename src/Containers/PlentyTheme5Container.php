a72ae6ab8bc02be4d8fbcdfe457cdc1f72dac6c2
Marc Lubitsch <marc.lubitsch@bookrix.com>
2020-05-03 13:33:36 +0200

    test

--- src/Containers/PlentyTheme5Container.php
+++ src/Containers/PlentyTheme5Container.php
@@ -6,8 +6,9 @@ use Plenty\Plugin\Templates\Twig;
 
 class PlentyTheme5Container
 {
-    public function call(Twig $twig):string
+    public function call(Twig $twig, $arg):string
     {
+        
         return $twig->render('PlentyTheme5::Stylesheet');
     }
 }
